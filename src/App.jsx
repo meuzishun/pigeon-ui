00529da7773048ef5446162f593273b560945136
Andrew Smith <meuzishun@gmail.com>
2025-01-27 15:43:50 -0500

    Create app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { routes } from './routes';
+import { LoadingModalProvider } from './contexts/LoadingModalContext';
+import { AuthProvider } from './contexts/AuthContext';
+import LoadingModal from './components/LoadingModal/LoadingModal';
+import AutoLogin from './AutoLogin';
+
+const router = createBrowserRouter(routes, { basename: '/pigeon-ui' });
+
 function App() {
-  return <></>;
+  return (
+    <LoadingModalProvider>
+      <AuthProvider>
+        <AutoLogin creds={{ email: 'andrew@email.com', password: '12345678' }}>
+          <LoadingModal />
+          <RouterProvider router={router} />
+        </AutoLogin>
+      </AuthProvider>
+    </LoadingModalProvider>
+  );
 }
 
 export default App;
