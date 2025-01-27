3bf1db3d89796b93fbd0da72122e4c07c9f7ca48
Andrew Smith <meuzishun@gmail.com>
2025-01-27 15:13:16 -0500

    Add constants from other repo

---
+++ src/constants/inputFields.js
@@ -0,0 +1,11 @@
+export const registerInputFields = [
+  { type: 'text', id: 'firstName', name: 'firstName', label: 'first name' },
+  { type: 'text', id: 'lastName', name: 'lastName', label: 'last name' },
+  { type: 'email', id: 'email', name: 'email', label: 'email' },
+  { type: 'password', id: 'password', name: 'password', label: 'password' },
+];
+
+export const loginInputFields = [
+  { type: 'email', id: 'email', name: 'email', label: 'email' },
+  { type: 'password', id: 'password', name: 'password', label: 'password' },
+];
---
+++ src/constants/navLinks.js
@@ -0,0 +1,13 @@
+// navLinks.js
+export const preAuthNavLinks = [
+  { textContent: 'welcome', url: '' },
+  { textContent: 'register', url: 'register' },
+  { textContent: 'login', url: 'login' },
+];
+
+export const postAuthNavLinks = [
+  { textContent: 'dashboard', url: 'messages' },
+  { textContent: 'profile', url: 'profile' },
+  { textContent: 'friends', url: 'friends' },
+  { textContent: 'logout', url: 'logout' },
+];
