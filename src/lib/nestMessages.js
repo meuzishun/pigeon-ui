0ea92962df8bc94f5cffe1d759b7516a0b0bdb95
Andrew Smith <meuzishun@gmail.com>
2025-01-31 15:26:37 -0500

    Adjust nestMessages function

--- src/lib/nestMessages.js
+++ src/lib/nestMessages.js
@@ -1,22 +1,22 @@
 export const nestMessages = (messages) => {
-  const messageMap = new Map();
+  const msgDict = {};
   const conversations = [];
 
-  messages.forEach((msg) => messageMap.set(msg.id, { ...msg, replies: [] }));
+  messages.forEach((msg) => (msgDict[msg._id] = msg));
 
   messages.forEach((msg) => {
     if (msg.parentId === null) {
-      conversations.push([messageMap.get(msg.id)]);
+      conversations.push([msg]);
     } else {
-      const parent = messageMap.get(msg.parentId);
+      const parent = msgDict[msg.parentId];
 
       if (parent) {
         const convo = conversations.find(
-          (c) => c[c.length - 1].id === msg.parentId
+          (c) => c[c.length - 1]._id === msg.parentId
         );
 
         if (convo) {
-          convo.push(messageMap.get(msg.id));
+          convo.push(msg);
         }
       }
     }
