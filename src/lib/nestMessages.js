1b99779ac98dc5b07657660a6e18ae5729f5a285
Andrew Smith <meuzishun@gmail.com>
2025-02-02 19:43:00 -0500

    Wrap nestMessages in a conditional

--- src/lib/nestMessages.js
+++ src/lib/nestMessages.js
@@ -2,25 +2,27 @@ export const nestMessages = (messages) => {
   const msgDict = {};
   const conversations = [];
 
-  messages.forEach((msg) => (msgDict[msg._id] = msg));
+  if (messages) {
+    messages.forEach((msg) => (msgDict[msg._id] = msg));
 
-  messages.forEach((msg) => {
-    if (msg.parentId === null) {
-      conversations.push([msg]);
-    } else {
-      const parent = msgDict[msg.parentId];
+    messages.forEach((msg) => {
+      if (msg.parentId === null) {
+        conversations.push([msg]);
+      } else {
+        const parent = msgDict[msg.parentId];
 
-      if (parent) {
-        const convo = conversations.find(
-          (c) => c[c.length - 1]._id === msg.parentId
-        );
+        if (parent) {
+          const convo = conversations.find(
+            (c) => c[c.length - 1]._id === msg.parentId
+          );
 
-        if (convo) {
-          convo.push(msg);
+          if (convo) {
+            convo.push(msg);
+          }
         }
       }
-    }
-  });
+    });
+  }
 
   return conversations;
 };
