93c5375e16842032219838c913be7b665c271f86
Andrew Smith <meuzishun@gmail.com>
2025-02-03 19:41:44 -0500

    Add dev api urls

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,2 +1,4 @@
-export const API_URL = 'pigeon-api-production.up.railway.app/api';
-export const SOCKET_URL = 'pigeon-api-production.up.railway.app';
+export const API_URL = 'https://pigeon-api-production.up.railway.app/api';
+// export const API_URL = 'http://localhost:3000/api';
+export const SOCKET_URL = 'https://pigeon-api-production.up.railway.app';
+// export const SOCKET_URL = 'http://localhost:3000';
